54c4eccb87ec25e7ad982279dff4facdee5a80c5
Santhosh-CSE <190030905cse@gmail.com>
2022-09-12 15:20:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+require('dotenv/config');
+
+app.use(bodyParser.json());
+
+const userRoutes = require('./routes/users');
+
+app.use('/users', userRoutes);
+
+//Routes
+app.get('/',(req,res)=>{
+    res.send('We are on home');
+});
+
+//Connecting with DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true}, () => 
+    console.log('Connected to DB')
+);
+
+//This will be listening the Server
+app.listen(3000);
