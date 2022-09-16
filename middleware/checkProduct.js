09af3b21ab46c30f50bf71eb435cbff376ccf374
Santhosh-CSE <190030905cse@gmail.com>
2022-09-16 14:58:16 +0530

    updated to store productid

--- middleware/checkProduct.js
+++ middleware/checkProduct.js
@@ -3,7 +3,7 @@ const schema = require('../models/DB/productschema');
 const checkProduct = async (req, res, next) => {
 const count = Object.keys(req.body.productid).length;
 console.log(count);
-for(var i=1; i<count; i++){
+for(var i=1; i<=count; i++){
     const temp = req.body.productid[i];
     try{
     const checkProduct1 = await schema.findOne({_id:temp});
